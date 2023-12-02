21b2da3181f5a35685135debfd23f04c26d51c3a
oupson <oupson1er@gmail.com>
2023-12-02 09:27:19 +0100

    Remove code duplication

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,47 +1,54 @@
 import kotlin.math.max
 
+fun Sequence<String>.extractPairAndSets() = this.map {
+    val split = it.split(": ")
+    Pair(
+        split.first().removePrefix("Game ").toInt(),
+        split.last()
+    )
+}
+
+fun Sequence<Pair<Int, String>>.extractSets() = this.map {
+    Pair(
+        it.first,
+        it.second.split("; ").map { set ->
+            val setValue = mutableMapOf<String, Int>()
+            set.split(", ").map { cube ->
+                val splitCube = cube.split(" ")
+                setValue[splitCube.last()] = (setValue[splitCube.last()] ?: 0) + splitCube.first().toInt()
+            }
+
+            setValue
+        }
+    )
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.asSequence().map {
-            it.split(": ").let {
-                Pair(it.first().removePrefix("Game ").toInt(), it.last().split("; ").map {
-                    val setValue = mutableMapOf<String, Int>()
-                    it.split(", ").map {
-                        val s = it.split(" ")
-                        setValue[s.last()] = (setValue[s.last()] ?: 0) + s.first().toInt()
-                    }
-
-                    setValue
-                })
-            }
-        }.filter {
-            it.second.none { set ->
-                set.getOrDefault("red", 0) > 12 || set.getOrDefault("green", 0) > 13 || set.getOrDefault("blue", 0) > 14
-            }
-        }.map { it.first }.sum()
+        return input.asSequence().extractPairAndSets()
+            .extractSets()
+            .filter {
+                it.second.none { set ->
+                    set.getOrDefault("red", 0) > 12 || set.getOrDefault("green", 0) > 13 || set.getOrDefault(
+                        "blue",
+                        0
+                    ) > 14
+                }
+            }.map { it.first }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.asSequence().map {
-            it.split(": ").let {
-                Pair(it.first().removePrefix("Game ").toInt(), it.last().split("; ").map {
-                    val setValue = mutableMapOf<String, Int>()
-                    it.split(", ").map {
-                        val s = it.split(" ")
-                        setValue[s.last()] = (setValue[s.last()] ?: 0) + s.first().toInt()
+        return input.asSequence()
+            .extractPairAndSets()
+            .extractSets()
+            .map {
+                it.second.fold(mutableMapOf<String, Int>()) { acc, set ->
+                    for (comp in set) {
+                        acc[comp.key] = max(acc[comp.key] ?: 0, comp.value)
                     }
-
-                    setValue
-                })
-            }
-        }.map {
-            it.second.fold(mutableMapOf<String, Int>()) { acc, set ->
-                for (comp in set) {
-                    acc[comp.key] = max(acc[comp.key] ?: 0, comp.value)
+                    acc
                 }
-                acc
-            }
-        }.map { it.values.fold(1) { acc, i -> acc * i } }.sum()
+            }.map { it.values.fold(1) { acc, i -> acc * i } }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
